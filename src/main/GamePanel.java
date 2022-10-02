e04365a11c3863c566c374dd1787e58e149e9938
NKDuy <kn145660@gmail.com>
2022-10-02 21:25:15 +0700

    chore: Display FPS

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -60,18 +60,27 @@ public class GamePanel extends JPanel implements Runnable {
         double delta = 0;
         long lastTime = System.nanoTime();
         long currentTime;
+        long timer = 0;
+        long drawCount = 0;
 
         while (gameThread != null) {
             currentTime = System.nanoTime();
 
             delta += (currentTime -lastTime) / drawInterval;
-
+            timer += (currentTime - lastTime);
             lastTime = currentTime;
 
             if (delta >= 1) {
                 update();
                 repaint();
                 delta--;
+                drawCount++;
+            }
+
+            if (timer >= 1000000000) {
+                System.out.println("FPS: " + drawCount);
+                drawCount = 0;
+                timer = 0;
             }
         }
     }
