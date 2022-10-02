60fd143aeab8ed1a1cc9c1b312af50a9480c308c
NKDuy <kn145660@gmail.com>
2022-10-02 20:37:12 +0700

    chore: Resolution

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,10 +1,26 @@
 package main;
 
 import javax.swing.*;
+import java.awt.*;
 
 // This class inherits JPanel class
 public class GamePanel extends JPanel {
 
     // SCREEN SETTINGS
     final int originalTileSize = 16; // 16x16 tile
+    final int scale = 3;
+
+    final int tileSize = originalTileSize * scale; // 48x48 tile
+    final int maxScreenCol = 16;
+    final int maxScreenRow = 12;
+    final int screenWidth = tileSize * maxScreenCol; // 768 pixels
+    final int screenHeight = tileSize * maxScreenRow; // 576 pixels
+
+    public GamePanel() {
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.black);
+        // If set to true, all the drawing from this component will be done in an offscreen painting buffer.
+        // In short, enabling this can improve game's rendering performance
+        this.setDoubleBuffered(true);
+    }
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -5,13 +5,20 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         JFrame window = new JFrame();
-        // This lets the window properly close when user clicks the close ("x") button
+        // This lets the window properly close when user clicks the close ("x") button.
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
         window.setTitle("2D Adventure");
 
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+
+        // *pack(): Cause this Window to be sized to fix the preferred size and layout of
+        // its subcomponents (= GamePanel).
+        window.pack();
+
         // Not specify the location of the window.
-        // = The window will be displayed at the center of the screen
+        // = The window will be displayed at the center of the screen.
         window.setLocationRelativeTo(null);
         window.setVisible(true);
     }
