8783996650a2e9f6323c4be4426fd904749040e3
NKDuy <kn145660@gmail.com>
2022-10-02 20:42:42 +0700

    chore: Game Mechanics

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 
 // This class inherits JPanel class
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable {
 
     // SCREEN SETTINGS
     final int originalTileSize = 16; // 16x16 tile
@@ -16,11 +16,34 @@ public class GamePanel extends JPanel {
     final int screenWidth = tileSize * maxScreenCol; // 768 pixels
     final int screenHeight = tileSize * maxScreenRow; // 576 pixels
 
+    Thread gameThread;
+
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         // If set to true, all the drawing from this component will be done in an offscreen painting buffer.
         // In short, enabling this can improve game's rendering performance
         this.setDoubleBuffered(true);
+    }
+
+    public void startGameThread() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    /**
+     * When an object implementing interface <code>Runnable</code> is used
+     * to create a thread, starting the thread causes the object's
+     * <code>run</code> method to be called in that separately executing
+     * thread.
+     * <p>
+     * The general contract of the method <code>run</code> is that it may
+     * take any action whatsoever.
+     *
+     * @see Thread#run()
+     */
+    @Override
+    public void run() {
+        //
     }
 }
