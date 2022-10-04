9526f133a0f01e818cf7f7404d5fa54fb2b29d81
NKDuy <kn145660@gmail.com>
2022-10-04 21:02:11 +0700

    chore: Importing tile images

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -25,11 +25,6 @@ public class GamePanel extends JPanel implements Runnable {
     Thread gameThread;
     Player player = new Player(this, keyH);
 
-    // Set player's default position
-    int playerX = 100;
-    int playerY = 100;
-    int playerSpeed = 4;
-
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
