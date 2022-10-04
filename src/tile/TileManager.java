f79e3ccada2fe25db3347c30d533a9f7285cf098
NKDuy <kn145660@gmail.com>
2022-10-04 21:07:43 +0700

    chore: Create Tile and TileManager class

---
+++ src/tile/TileManager.java
@@ -0,0 +1,40 @@
+package tile;
+
+import main.GamePanel;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.io.IOException;
+
+public class TileManager {
+
+    GamePanel gp;
+    Tile[] tile;
+
+    public TileManager(GamePanel gp) {
+        this.gp = gp;
+
+        tile = new Tile[10];
+
+        getTileImage();
+    }
+
+    public void getTileImage() {
+        try {
+            tile[0] = new Tile();
+            tile[0].image = ImageIO.read(getClass().getResourceAsStream("/tiles/grass.png"));
+
+            tile[1] = new Tile();
+            tile[1].image = ImageIO.read(getClass().getResourceAsStream("/tiles/wall.png"));
+
+            tile[2] = new Tile();
+            tile[2].image = ImageIO.read(getClass().getResourceAsStream("/tiles/water.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void draw(Graphics2D g2) {
+        //
+    }
+}
