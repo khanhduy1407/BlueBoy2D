6c6da5ce6c540468ab60863066b331e6f7fdaf87
NKDuy <kn145660@gmail.com>
2022-10-07 18:17:16 +0700

    chore: Display a key image

--- src/main/UI.java
+++ src/main/UI.java
@@ -1,21 +1,29 @@
 package main;
 
+import object.OBJ_Key;
+
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public class UI {
 
     GamePanel gp;
     Font arial_40;
+    BufferedImage keyImage;
 
     public UI(GamePanel gp) {
         this.gp = gp;
 
         arial_40 = new Font("Arial", Font.PLAIN, 40);
+
+        OBJ_Key key = new OBJ_Key();
+        keyImage = key.image;
     }
 
     public void draw(Graphics2D g2) {
         g2.setFont(arial_40);
         g2.setColor(Color.white);
-        g2.drawString("Key = " + gp.player.hasKey, 25, 50);
+        g2.drawImage(keyImage, gp.tileSize/2, gp.tileSize/2, gp.tileSize, gp.tileSize, null);
+        g2.drawString("x " + gp.player.hasKey, 74, 65);
     }
 }
