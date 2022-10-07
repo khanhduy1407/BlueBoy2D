c0023248b668bef47eafb6c68eea0a4ea9015609
NKDuy <kn145660@gmail.com>
2022-10-07 18:05:35 +0700

    chore: Create UI class

---
+++ src/main/UI.java
@@ -0,0 +1,16 @@
+package main;
+
+import java.awt.*;
+
+public class UI {
+
+    GamePanel gp;
+
+    public UI(GamePanel gp) {
+        this.gp = gp;
+    }
+
+    public void draw(Graphics2D g2) {
+        //
+    }
+}
