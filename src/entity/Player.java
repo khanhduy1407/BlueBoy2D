bb8a843a1f0851d1bcf05ad543501c5ac8699514
NKDuy <kn145660@gmail.com>
2022-10-07 18:00:32 +0700

    chore: Play sound effect

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -111,18 +111,21 @@ public class Player extends Entity {
 
             switch (objectName) {
                 case "Key":
+                    gp.playSE(1);
                     hasKey++;
                     gp.obj[i] = null;
                     System.out.println("Key: " + hasKey);
                     break;
                 case "Door":
                     if (hasKey > 0) {
+                        gp.playSE(3);
                         gp.obj[i] = null;
                         hasKey--;
                     }
                     System.out.println("Key: " + hasKey);
                     break;
                 case "Boots": // Increase player's speed
+                    gp.playSE(2);
                     speed += 1;
                     gp.obj[i] = null;
                     break;
