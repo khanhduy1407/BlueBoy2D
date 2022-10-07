b2c43751d981a0547e9288edb8433fb2279694a1
NKDuy <kn145660@gmail.com>
2022-10-07 18:40:00 +0700

    chore: Display text at the center

--- src/main/UI.java
+++ src/main/UI.java
@@ -8,7 +8,7 @@ import java.awt.image.BufferedImage;
 public class UI {
 
     GamePanel gp;
-    Font arial_40;
+    Font arial_40, arial_80B;
     BufferedImage keyImage;
     public boolean messageOn = false;
     public String message = "";
@@ -19,6 +19,7 @@ public class UI {
         this.gp = gp;
 
         arial_40 = new Font("Arial", Font.PLAIN, 40);
+        arial_80B = new Font("Arial", Font.BOLD, 80);
 
         OBJ_Key key = new OBJ_Key();
         keyImage = key.image;
@@ -31,7 +32,29 @@ public class UI {
 
     public void draw(Graphics2D g2) {
         if (gameFinished == true) {
-            //
+            g2.setFont(arial_40);
+            g2.setColor(Color.white);
+
+            String text;
+            int textLength;
+            int x;
+            int y;
+
+            text = "You found the treasure!";
+            textLength = (int) g2.getFontMetrics().getStringBounds(text, g2).getWidth();
+            x = gp.screenWidth/2 - textLength/2;
+            y = gp.screenHeight/2 - (gp.tileSize*3);
+            g2.drawString(text, x, y);
+
+            g2.setFont(arial_80B);
+            g2.setColor(Color.yellow);
+            text = "Congratulations!";
+            textLength = (int) g2.getFontMetrics().getStringBounds(text, g2).getWidth();
+            x = gp.screenWidth/2 - textLength/2;
+            y = gp.screenHeight/2 + (gp.tileSize*2);
+            g2.drawString(text, x, y);
+
+            gp.gameThread = null;
         } else {
             g2.setFont(arial_40);
             g2.setColor(Color.white);
