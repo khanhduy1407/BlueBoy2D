7b7d5bb9bc7c11e36e1895229e24af9a50523534
NKDuy <kn145660@gmail.com>
2022-10-07 18:42:45 +0700

    chore: Display play time

--- src/main/UI.java
+++ src/main/UI.java
@@ -15,6 +15,8 @@ public class UI {
     int messageCounter = 0;
     public boolean gameFinished = false;
 
+    double playTime;
+
     public UI(GamePanel gp) {
         this.gp = gp;
 
@@ -60,6 +62,10 @@ public class UI {
             g2.setColor(Color.white);
             g2.drawImage(keyImage, gp.tileSize / 2, gp.tileSize / 2, gp.tileSize, gp.tileSize, null);
             g2.drawString("x " + gp.player.hasKey, 74, 65);
+
+            // TIME
+            playTime += (double) 1/60;
+            g2.drawString("Time: " + playTime, gp.tileSize*11, 65);
 
             // MESSAGE
             if (messageOn == true) {
