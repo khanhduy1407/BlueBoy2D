923bc4e56804663a73b3aabaa852e137404eea1d
NKDuy <kn145660@gmail.com>
2023-01-21 23:03:08 +0700

    test: Checking how long it is taking to draw

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -115,6 +115,12 @@ public class GamePanel extends JPanel implements Runnable {
 
         Graphics2D g2 = (Graphics2D) g;
 
+        // DEBUG
+        long drawStart = 0;
+        if (keyH.checkDrawTime) {
+            drawStart = System.nanoTime();
+        }
+
         // TILE
         tileM.draw(g2);
 
@@ -131,6 +137,15 @@ public class GamePanel extends JPanel implements Runnable {
 
         // UI
         ui.draw(g2);
+
+        // DEBUG
+        if (keyH.checkDrawTime) {
+            long drawEnd = System.nanoTime();
+            long passed = drawEnd - drawStart;
+            g2.setColor(Color.white);
+            g2.drawString("Draw Time: " + passed, 10, 400);
+            System.out.println("Draw Time: " + passed);
+        }
 
         // Dispose of this graphics context and release any system resources that it is using.
         g2.dispose();
--- src/main/KeyHandler.java
+++ src/main/KeyHandler.java
@@ -6,6 +6,8 @@ import java.awt.event.KeyListener;
 public class KeyHandler implements KeyListener {
 
     public boolean upPressed, downPressed, leftPressed, rightPressed;
+    // DEBUG
+    boolean checkDrawTime = false;
 
     /**
      * Invoked when a key has been typed.
@@ -39,6 +41,15 @@ public class KeyHandler implements KeyListener {
         }
         if (code == KeyEvent.VK_D) {
             rightPressed = true;
+        }
+
+        // DEBUG
+        if (code == KeyEvent.VK_T) {
+            if (!checkDrawTime) {
+                checkDrawTime = true;
+            } else if (checkDrawTime) {
+                checkDrawTime = false;
+            }
         }
     }
 
