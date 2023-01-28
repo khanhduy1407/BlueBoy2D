1c89f912917e78661c5ef2d63d3818d9ad86c377
NKDuy <kn145660@gmail.com>
2023-01-28 20:05:00 +0700

    chore: Extra Section (Class Selection Screen)

--- src/main/KeyHandler.java
+++ src/main/KeyHandler.java
@@ -24,26 +24,60 @@ public class KeyHandler implements KeyListener {
 
         // TITLE STATE
         if (gp.gameState == gp.titleState) {
-            if (code == KeyEvent.VK_W) {
-                if (gp.ui.commandNum > 0) {
-                    gp.ui.commandNum--;
+            if (gp.ui.titleScreenState == 0) {
+                if (code == KeyEvent.VK_W) {
+                    if (gp.ui.commandNum > 0) {
+                        gp.ui.commandNum--;
+                    }
+                }
+                if (code == KeyEvent.VK_S) {
+                    if (gp.ui.commandNum < 2) {
+                        gp.ui.commandNum++;
+                    }
+                }
+                if (code == KeyEvent.VK_ENTER) {
+                    if (gp.ui.commandNum == 0) {
+                        gp.ui.titleScreenState = 1;
+                    }
+                    if (gp.ui.commandNum == 1) {
+                        // add later
+                    }
+                    if (gp.ui.commandNum == 2) {
+                        System.exit(0);
+                    }
                 }
             }
-            if (code == KeyEvent.VK_S) {
-                if (gp.ui.commandNum < 2) {
-                    gp.ui.commandNum++;
+            else if (gp.ui.titleScreenState == 1) {
+                if (code == KeyEvent.VK_W) {
+                    if (gp.ui.commandNum > 0) {
+                        gp.ui.commandNum--;
+                    }
                 }
-            }
-            if (code == KeyEvent.VK_ENTER) {
-                if (gp.ui.commandNum == 0) {
-                    gp.gameState = gp.playState;
-                    gp.playMusic(0);
+                if (code == KeyEvent.VK_S) {
+                    if (gp.ui.commandNum < 3) {
+                        gp.ui.commandNum++;
+                    }
                 }
-                if (gp.ui.commandNum == 1) {
-                    // add later
-                }
-                if (gp.ui.commandNum == 2) {
-                    System.exit(0);
+                if (code == KeyEvent.VK_ENTER) {
+                    if (gp.ui.commandNum == 0) {
+                        System.out.println("Do some fighter specific stuff!");
+                        gp.gameState = gp.playState;
+                        gp.playMusic(0);
+                    }
+                    if (gp.ui.commandNum == 1) {
+                        System.out.println("Do some thief specific stuff!");
+                        gp.gameState = gp.playState;
+                        gp.playMusic(0);
+                    }
+                    if (gp.ui.commandNum == 2) {
+                        System.out.println("Do some sorcerer specific stuff!");
+                        gp.gameState = gp.playState;
+                        gp.playMusic(0);
+                    }
+                    if (gp.ui.commandNum == 3) {
+                        gp.ui.titleScreenState = 0;
+                        gp.ui.commandNum = 0;
+                    }
                 }
             }
         }
--- src/main/UI.java
+++ src/main/UI.java
@@ -19,6 +19,7 @@ public class UI {
     public boolean gameFinished = false;
     public String currentDialogue = "";
     public int commandNum = 0;
+    public int titleScreenState = 0; // 0: the first screen, 1: the second screen
 
     public UI(GamePanel gp) {
         this.gp = gp;
@@ -63,52 +64,95 @@ public class UI {
     }
 
     public void drawTitleScreen() {
-        g2.setColor(new Color(0, 0, 0));
-        g2.fillRect(0, 0, gp.screenWidth, gp.screenHeight);
+        if (titleScreenState == 0) {
+            g2.setColor(new Color(0, 0, 0));
+            g2.fillRect(0, 0, gp.screenWidth, gp.screenHeight);
 
-        // TITLE NAME
-        g2.setFont(g2.getFont().deriveFont(Font.BOLD, 96F));
-        String text = "Blue Boy Adventure";
-        int x = getXForCenteredText(text);
-        int y = gp.tileSize * 3;
+            // TITLE NAME
+            g2.setFont(g2.getFont().deriveFont(Font.BOLD, 96F));
+            String text = "Blue Boy Adventure";
+            int x = getXForCenteredText(text);
+            int y = gp.tileSize * 3;
 
-        // SHADOW
-        g2.setColor(Color.gray);
-        g2.drawString(text, x + 5, y + 5);
-        // MAIN COLOR
-        g2.setColor(Color.white);
-        g2.drawString(text, x, y);
+            // SHADOW
+            g2.setColor(Color.gray);
+            g2.drawString(text, x + 5, y + 5);
+            // MAIN COLOR
+            g2.setColor(Color.white);
+            g2.drawString(text, x, y);
 
-        // BLUE BOY IMAGE
-        x = gp.screenWidth / 2 - (gp.tileSize * 2) / 2;
-        y += gp.tileSize * 2;
-        g2.drawImage(gp.player.down1, x, y, gp.tileSize * 2, gp.tileSize * 2, null);
+            // BLUE BOY IMAGE
+            x = gp.screenWidth / 2 - (gp.tileSize * 2) / 2;
+            y += gp.tileSize * 2;
+            g2.drawImage(gp.player.down1, x, y, gp.tileSize * 2, gp.tileSize * 2, null);
 
-        // MENU
-        g2.setFont(g2.getFont().deriveFont(Font.BOLD, 48F));
+            // MENU
+            g2.setFont(g2.getFont().deriveFont(Font.BOLD, 48F));
 
-        text = "NEW GAME";
-        x = getXForCenteredText(text);
-        y += gp.tileSize * 3.5;
-        g2.drawString(text, x, y);
-        if (commandNum == 0) {
-            g2.drawString(">", x - gp.tileSize, y);
-        }
+            text = "NEW GAME";
+            x = getXForCenteredText(text);
+            y += gp.tileSize * 3.5;
+            g2.drawString(text, x, y);
+            if (commandNum == 0) {
+                g2.drawString(">", x - gp.tileSize, y);
+            }
 
-        text = "LOAD GAME";
-        x = getXForCenteredText(text);
-        y += gp.tileSize;
-        g2.drawString(text, x, y);
-        if (commandNum == 1) {
-            g2.drawString(">", x - gp.tileSize, y);
-        }
+            text = "LOAD GAME";
+            x = getXForCenteredText(text);
+            y += gp.tileSize;
+            g2.drawString(text, x, y);
+            if (commandNum == 1) {
+                g2.drawString(">", x - gp.tileSize, y);
+            }
 
-        text = "QUIT";
-        x = getXForCenteredText(text);
-        y += gp.tileSize;
-        g2.drawString(text, x, y);
-        if (commandNum == 2) {
-            g2.drawString(">", x - gp.tileSize, y);
+            text = "QUIT";
+            x = getXForCenteredText(text);
+            y += gp.tileSize;
+            g2.drawString(text, x, y);
+            if (commandNum == 2) {
+                g2.drawString(">", x - gp.tileSize, y);
+            }
+        } else if (titleScreenState == 1) {
+            // CLASS SELECTION SCREEN
+            g2.setColor(Color.white);
+            g2.setFont(g2.getFont().deriveFont(42F));
+
+            String text = "Select your class!";
+            int x = getXForCenteredText(text);
+            int y = gp.tileSize * 3;
+            g2.drawString(text, x, y);
+
+            text = "Fighter";
+            x = getXForCenteredText(text);
+            y += gp.tileSize * 3;
+            g2.drawString(text, x, y);
+            if (commandNum == 0) {
+                g2.drawString(">", x - gp.tileSize, y);
+            }
+
+            text = "Thief";
+            x = getXForCenteredText(text);
+            y += gp.tileSize;
+            g2.drawString(text, x, y);
+            if (commandNum == 1) {
+                g2.drawString(">", x - gp.tileSize, y);
+            }
+
+            text = "Sorcerer";
+            x = getXForCenteredText(text);
+            y += gp.tileSize;
+            g2.drawString(text, x, y);
+            if (commandNum == 2) {
+                g2.drawString(">", x - gp.tileSize, y);
+            }
+
+            text = "Back";
+            x = getXForCenteredText(text);
+            y += gp.tileSize * 2;
+            g2.drawString(text, x, y);
+            if (commandNum == 3) {
+                g2.drawString(">", x - gp.tileSize, y);
+            }
         }
     }
 
