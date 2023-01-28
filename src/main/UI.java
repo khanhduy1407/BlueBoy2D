1f015d7b597a0ff05f46184ff4a7deea68480614
NKDuy <kn145660@gmail.com>
2023-01-28 08:31:28 +0700

    chore: House-keeping 1

--- src/main/UI.java
+++ src/main/UI.java
@@ -10,7 +10,6 @@ public class UI {
 
     GamePanel gp;
     Font arial_40, arial_80B;
-    BufferedImage keyImage;
     public boolean messageOn = false;
     public String message = "";
     int messageCounter = 0;
@@ -24,9 +23,6 @@ public class UI {
 
         arial_40 = new Font("Arial", Font.PLAIN, 40);
         arial_80B = new Font("Arial", Font.BOLD, 80);
-
-        OBJ_Key key = new OBJ_Key(gp);
-        keyImage = key.image;
     }
 
     public void showMessage(String text) {
@@ -35,58 +31,6 @@ public class UI {
     }
 
     public void draw(Graphics2D g2) {
-        if (gameFinished == true) {
-            g2.setFont(arial_40);
-            g2.setColor(Color.white);
-
-            String text;
-            int textLength;
-            int x;
-            int y;
-
-            text = "You found the treasure!";
-            textLength = (int) g2.getFontMetrics().getStringBounds(text, g2).getWidth();
-            x = gp.screenWidth/2 - textLength/2;
-            y = gp.screenHeight/2 - (gp.tileSize*3);
-            g2.drawString(text, x, y);
-
-            text = "Your time is: " + dFormat.format(playTime) + "s!";
-            textLength = (int) g2.getFontMetrics().getStringBounds(text, g2).getWidth();
-            x = gp.screenWidth/2 - textLength/2;
-            y = gp.screenHeight/2 + (gp.tileSize*4);
-            g2.drawString(text, x, y);
-
-            g2.setFont(arial_80B);
-            g2.setColor(Color.yellow);
-            text = "Congratulations!";
-            textLength = (int) g2.getFontMetrics().getStringBounds(text, g2).getWidth();
-            x = gp.screenWidth/2 - textLength/2;
-            y = gp.screenHeight/2 + (gp.tileSize*2);
-            g2.drawString(text, x, y);
-
-            gp.gameThread = null;
-        } else {
-            g2.setFont(arial_40);
-            g2.setColor(Color.white);
-            g2.drawImage(keyImage, gp.tileSize / 2, gp.tileSize / 2, gp.tileSize, gp.tileSize, null);
-            g2.drawString("x " + gp.player.hasKey, 74, 65);
-
-            // TIME
-            playTime += (double) 1/60;
-            g2.drawString("Time: " + dFormat.format(playTime), gp.tileSize*11, 65);
-
-            // MESSAGE
-            if (messageOn == true) {
-                g2.setFont(g2.getFont().deriveFont(30F));
-                g2.drawString(message, gp.tileSize / 2, gp.tileSize * 5);
-
-                messageCounter++;
-
-                if (messageCounter > 120) {
-                    messageCounter = 0;
-                    messageOn = false;
-                }
-            }
-        }
+        //
     }
 }
