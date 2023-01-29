10120faac14d6f67fb8c83ff8a0c7f7c356d0196
NKDuy <kn145660@gmail.com>
2023-01-29 17:58:47 +0700

    chore: Preparing attack images

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -21,6 +21,8 @@ public class Entity {
     // It describes an Image with an accessible buffer of image data
     // (We use this to store our image files)
     public BufferedImage up1, up2, down1, down2, left1, left2, right1, right2;
+    public BufferedImage attackUp1, attackUp2, attackDown1, attackDown2,
+                         attackLeft1, attackLeft2, attackRight1, attackRight2;
     public String direction = "down";
 
     public int spriteCounter = 0;
