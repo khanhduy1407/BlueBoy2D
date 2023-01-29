0bbc423d4195fe0e1d581adcf366ab9829b8fcb2
NKDuy <kn145660@gmail.com>
2023-01-29 10:46:18 +0700

    chore: Replace Rectangle with EventRect

--- src/main/EventHandler.java
+++ src/main/EventHandler.java
@@ -23,24 +23,30 @@ public class EventHandler {
             eventRect[col][row].height = 2;
             eventRect[col][row].eventRectDefaultX = eventRect[col][row].x;
             eventRect[col][row].eventRectDefaultY = eventRect[col][row].y;
+
+            col++;
+            if (col < gp.maxWorldCol) {
+                col = 0;
+                row++;
+            }
         }
     }
 
     public void checkEvent() {
-//        if (hit(27, 16, "right")) { damagePit(gp.dialogueState); }
-        if (hit(27, 16, "right")) { teleport(gp.dialogueState); }
+        if (hit(27, 16, "right")) { damagePit(gp.dialogueState); }
+//        if (hit(27, 16, "right")) { teleport(gp.dialogueState); }
         if (hit(23, 12, "up")) { healingPool(gp.dialogueState); }
     }
 
-    public boolean hit(int eventCol, int eventRow, String reqDirection) {
+    public boolean hit(int col, int row, String reqDirection) {
         boolean hit = false;
 
         gp.player.solidArea.x = gp.player.worldX + gp.player.solidArea.x;
         gp.player.solidArea.y = gp.player.worldY + gp.player.solidArea.y;
-        eventRect.x = eventCol * gp.tileSize + eventRect.x;
-        eventRect.y = eventRow * gp.tileSize + eventRect.y;
+        eventRect[col][row].x = col * gp.tileSize + eventRect[col][row].x;
+        eventRect[col][row].y = row * gp.tileSize + eventRect[col][row].y;
 
-        if (gp.player.solidArea.intersects(eventRect)) {
+        if (gp.player.solidArea.intersects(eventRect[col][row])) {
             if (gp.player.direction.contentEquals(reqDirection) || reqDirection.contentEquals("any")) {
                 hit = true;
             }
@@ -48,8 +54,8 @@ public class EventHandler {
 
         gp.player.solidArea.x = gp.player.solidAreaDefaultX;
         gp.player.solidArea.y = gp.player.solidAreaDefaultY;
-        eventRect.x = eventRectDefaultX;
-        eventRect.y = eventRectDefaultY;
+        eventRect[col][row].x = eventRect[col][row].eventRectDefaultX;
+        eventRect[col][row].y = eventRect[col][row].eventRectDefaultY;
 
         return hit;
     }
