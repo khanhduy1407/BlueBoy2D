07bc356c91f6b92e210d56ae80f23db868436ee3
NKDuy <kn145660@gmail.com>
2023-01-29 15:28:02 +0700

    chore: Setting Slime's behavior

--- src/monster/MON_GreenSlime.java
+++ src/monster/MON_GreenSlime.java
@@ -3,6 +3,8 @@ package monster;
 import entity.Entity;
 import main.GamePanel;
 
+import java.util.Random;
+
 public class MON_GreenSlime extends Entity {
 
     public MON_GreenSlime(GamePanel gp) {
@@ -19,6 +21,8 @@ public class MON_GreenSlime extends Entity {
         solidArea.height = 30;
         solidAreaDefaultX = solidArea.x;
         solidAreaDefaultY = solidArea.y;
+
+        getImage();
     }
 
     public void getImage() {
@@ -31,5 +35,29 @@ public class MON_GreenSlime extends Entity {
         left2 = setup("/monster/greenslime_down_2");
         right1 = setup("/monster/greenslime_down_1");
         right2 = setup("/monster/greenslime_down_2");
+    }
+
+    public void setAction() {
+        actionLockCounter++;
+
+        if (actionLockCounter == 120) { // 2 second
+            Random random = new Random();
+            int i = random.nextInt(100) + 1; // pick up a number from 1 to 100
+
+            if (i <= 25) {
+                direction = "up";
+            }
+            if (i > 25 && i <= 50) {
+                direction = "down";
+            }
+            if (i > 50 && i <= 75) {
+                direction = "left";
+            }
+            if (i > 75 && i <= 100) {
+                direction = "right";
+            }
+
+            actionLockCounter = 0;
+        }
     }
 }
