5c79704ea885dd1e549a0e050485a0cbe1f78db4
NKDuy <kn145660@gmail.com>
2023-01-29 15:19:29 +0700

    chore: Create Slime class

--- src/monster/MON_GreenSlime.java
+++ src/monster/MON_GreenSlime.java
@@ -1,4 +1,16 @@
 package monster;
 
-public class MON_GreenSlime {
+import entity.Entity;
+import main.GamePanel;
+
+public class MON_GreenSlime extends Entity {
+
+    public MON_GreenSlime(GamePanel gp) {
+        super(gp);
+
+        name = "Green Slime";
+        speed = 1;
+        maxLife = 4;
+        life = maxLife;
+    }
 }
