f84b99e7c7f101d0bdcd89d7d677785b77deefc9
NKDuy <kn145660@gmail.com>
2023-01-30 08:34:00 +0700

    fix: Stop player swinging the sword when the game starts

--- src/main/KeyHandler.java
+++ src/main/KeyHandler.java
@@ -84,7 +84,7 @@ public class KeyHandler implements KeyListener {
             }
         }
         // PLAY STATE
-        if (gp.gameState == gp.playState) {
+        else if (gp.gameState == gp.playState) {
             if (code == KeyEvent.VK_W) {
                 upPressed = true;
             }
