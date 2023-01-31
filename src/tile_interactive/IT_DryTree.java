5ace6521f30f94c79c61529eb9af25f3fb664bb3
NKDuy <kn145660@gmail.com>
2023-01-31 19:48:25 +0700

    chore: Dry Tree class

--- src/tile_interactive/IT_DryTree.java
+++ src/tile_interactive/IT_DryTree.java
@@ -1,4 +1,16 @@
 package tile_interactive;
 
-public class IT_DryTree {
+import main.GamePanel;
+
+public class IT_DryTree extends InteractiveTile {
+
+    GamePanel gp;
+
+    public IT_DryTree(GamePanel gp) {
+        super(gp);
+        this.gp = gp;
+
+        down1 = setup("/tiles_interactive/drytree", gp.tileSize, gp.tileSize);
+        destructible = true;
+    }
 }
