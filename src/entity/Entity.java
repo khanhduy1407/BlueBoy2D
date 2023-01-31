b996901f286950046293eacc277e8af53c54d8de
NKDuy <kn145660@gmail.com>
2023-01-31 16:04:57 +0700

    chore: Housekeeping

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -126,17 +126,7 @@ public class Entity {
         boolean contactPlayer = gp.cChecker.checkPlayer(this);
 
         if (this.type == type_monster && contactPlayer == true) {
-            if (gp.player.invincible == false) {
-                // we can give damage
-                gp.playSE(6);
-
-                int damage = attack - gp.player.defense;
-                if (damage < 0) {
-                    damage = 0;
-                }
-                gp.player.life -= damage;
-                gp.player.invincible = true;
-            }
+            damagePlayer(attack);
         }
 
         // IF COLLISION IS FALSE, NPC CAN MOVE
@@ -165,6 +155,24 @@ public class Entity {
                 invincible = false;
                 invincibleCounter = 0;
             }
+        }
+
+        if (shotAvailableCounter < 30) {
+            shotAvailableCounter++;
+        }
+    }
+
+    public void damagePlayer(int attack) {
+        if (gp.player.invincible == false) {
+            // we can give damage
+            gp.playSE(6);
+
+            int damage = attack - gp.player.defense;
+            if (damage < 0) {
+                damage = 0;
+            }
+            gp.player.life -= damage;
+            gp.player.invincible = true;
         }
     }
 
