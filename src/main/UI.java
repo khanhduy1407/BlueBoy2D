2c6d6a656be92ec5ce1b798ee6a9821cd4b44990
NKDuy <kn145660@gmail.com>
2023-01-31 16:31:51 +0700

    chore: Display player mana on the screen

--- src/main/UI.java
+++ src/main/UI.java
@@ -2,6 +2,7 @@ package main;
 
 import entity.Entity;
 import object.OBJ_Heart;
+import object.OBJ_ManaCrystal;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
@@ -14,7 +15,7 @@ public class UI {
     GamePanel gp;
     Graphics2D g2;
     Font maruMonica, purisaB;
-    BufferedImage heart_full, heart_half, heart_blank;
+    BufferedImage heart_full, heart_half, heart_blank, crystal_full, crystal_blank;
     public boolean messageOn = false;
     ArrayList<String> message = new ArrayList<>();
     ArrayList<Integer> messageCounter = new ArrayList<>();
@@ -42,6 +43,10 @@ public class UI {
         heart_full = heart.image;
         heart_half = heart.image2;
         heart_blank = heart.image3;
+
+        Entity mana = new OBJ_ManaCrystal(gp);
+        crystal_full = mana.image;
+        crystal_blank = mana.image2;
     }
 
     public void addMessage(String text) {
@@ -107,6 +112,26 @@ public class UI {
             }
             i++;
             x += gp.tileSize;
+        }
+
+        // DRAW MAX MANA
+        x = (gp.tileSize / 2) - 5;
+        y = (int) (gp.tileSize * 1.5);
+        i = 0;
+        while (i < gp.player.maxMana) {
+            g2.drawImage(crystal_blank, x, y, null);
+            i++;
+            x += 35;
+        }
+
+        // DRAW MANA
+        x = (gp.tileSize / 2) - 5;
+        y = (int) (gp.tileSize * 1.5);
+        i = 0;
+        while (i < gp.player.mana) {
+            g2.drawImage(crystal_full, x, y, null);
+            i++;
+            x += 35;
         }
     }
 
