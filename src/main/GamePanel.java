8dae69efb8c88b628885495d91411423079142d9
NKDuy <kn145660@gmail.com>
2023-02-01 09:16:08 +0700

    chore: Changing the Screen Ratio

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -20,9 +20,9 @@ public class GamePanel extends JPanel implements Runnable {
     final int scale = 3;
 
     public final int tileSize = originalTileSize * scale; // 48x48 tile
-    public final int maxScreenCol = 16;
+    public final int maxScreenCol = 20;
     public final int maxScreenRow = 12;
-    public final int screenWidth = tileSize * maxScreenCol; // 768 pixels
+    public final int screenWidth = tileSize * maxScreenCol; // 960 pixels
     public final int screenHeight = tileSize * maxScreenRow; // 576 pixels
 
     // WORLD SETTINGS
