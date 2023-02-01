9c79ac08e74815bf734bcb75f5331d4cf07c924b
NKDuy <kn145660@gmail.com>
2023-02-01 08:59:59 +0700

    chore: Disable Half Transparent Effect

--- src/tile_interactive/InteractiveTile.java
+++ src/tile_interactive/InteractiveTile.java
@@ -3,6 +3,9 @@ package tile_interactive;
 import entity.Entity;
 import main.GamePanel;
 
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
 public class InteractiveTile extends Entity {
 
     GamePanel gp;
@@ -33,6 +36,19 @@ public class InteractiveTile extends Entity {
                 invincible = false;
                 invincibleCounter = 0;
             }
+        }
+    }
+
+    public void draw(Graphics2D g2) {
+        int screenX = worldX - gp.player.worldX + gp.player.screenX;
+        int screenY = worldY - gp.player.worldY + gp.player.screenY;
+
+        if (worldX + gp.tileSize > gp.player.worldX - gp.player.screenX &&
+                worldX - gp.tileSize < gp.player.worldX + gp.player.screenX &&
+                worldY + gp.tileSize > gp.player.worldY - gp.player.screenY &&
+                worldY - gp.tileSize < gp.player.worldY + gp.player.screenY) {
+
+            g2.drawImage(down1, screenX, screenY, null);
         }
     }
 }
