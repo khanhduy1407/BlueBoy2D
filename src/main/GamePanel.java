f6268a0f91b543828121e6ed70c78ead10bd7e83
NKDuy <kn145660@gmail.com>
2023-02-03 09:23:12 +0700

    chore: Creating Pathfinder class

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -43,7 +43,7 @@ public class GamePanel extends JPanel implements Runnable {
     int FPS = 60;
 
     // SYSTEM
-    TileManager tileM = new TileManager(this);
+    public TileManager tileM = new TileManager(this);
     public KeyHandler keyH = new KeyHandler(this);
     Sound music = new Sound();
     Sound se = new Sound();
