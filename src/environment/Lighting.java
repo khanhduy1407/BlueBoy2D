412fe5f8bc91ef2b41cae7c6c9ee998774360aa5
NKDuy <kn145660@gmail.com>
2023-02-04 19:02:37 +0700

    chore: Code Update

--- src/environment/Lighting.java
+++ src/environment/Lighting.java
@@ -18,25 +18,9 @@ public class Lighting {
         darknessFilter = new BufferedImage(gp.screenWidth, gp.screenHeight, BufferedImage.TYPE_INT_ARGB);
         Graphics2D g2 = (Graphics2D) darknessFilter.getGraphics();
 
-        // Create a screen-sized rectangle ara
-        Area screenArea = new Area(new Rectangle2D.Double(0, 0, gp.screenWidth, gp.screenHeight));
-
         // Get the center x and y of the light circle
         int centerX = gp.player.screenX + (gp.tileSize)/2;
         int centerY = gp.player.screenY + (gp.tileSize)/2;
-
-        // Get the top left x and y of the light circle
-        double x = centerX - (circleSize)/2;
-        double y = centerY - (circleSize)/2;
-
-        // Create a light circle shape
-        Shape circleShape = new Ellipse2D.Double(x, y, circleSize, circleSize);
-
-        // Create a light circle area
-        Area lightArea = new Area(circleShape);
-
-        // Subtract the light circle from the screen rectangle
-        screenArea.subtract(lightArea);
 
         // Create a gradient effect within the light circle
         Color color[] = new Color[12];
@@ -74,14 +58,7 @@ public class Lighting {
         // Set the gradient data on g2
         g2.setPaint(gPaint);
 
-        // Draw the light circle
-        g2.fill(lightArea);
-
-        // Set a color (black) to draw the rectangle
-//        g2.setColor(new Color(0, 0, 0, 0.95f));
-
-        // Draw the screen rectangle without the light circle area
-        g2.fill(screenArea);
+        g2.fillRect(0, 0, gp.screenWidth, gp.screenHeight);
 
         g2.dispose();
     }
