530edd8b2b4eaddda65821e575ab1c150e6c07eb
NKDuy <kn145660@gmail.com>
2023-02-04 20:35:53 +0700

    chore: Creating Lantern class

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -69,6 +69,7 @@ public class Entity {
     public int coin;
     public Entity currentWeapon;
     public Entity currentShield;
+    public Entity currentLight;
     public Projectile projectile;
 
     // ITEM ATTRIBUTES
@@ -83,6 +84,7 @@ public class Entity {
     public int knockBackPower = 0;
     public boolean stackable = false;
     public int amount = 1;
+    public int lightRadius;
 
     // TYPE
     public int type;
@@ -95,6 +97,7 @@ public class Entity {
     public final int type_consumable = 6;
     public final int type_pickupOnly = 7;
     public final int type_obstacle = 8;
+    public final int type_light= 9;
 
     public Entity(GamePanel gp) {
         this.gp = gp;
---
+++ src/object/OBJ_Lantern.java
@@ -0,0 +1,18 @@
+package object;
+
+import entity.Entity;
+import main.GamePanel;
+
+public class OBJ_Lantern extends Entity {
+
+    public OBJ_Lantern(GamePanel gp) {
+        super(gp);
+
+        type = type_light;
+        name = "Lantern";
+        down1 = setup("/objects/lantern", gp.tileSize, gp.tileSize);
+        description = "[" + name + "]\nIlluminates your\nsurroundings.";
+        price = 200;
+        lightRadius = 250;
+    }
+}
