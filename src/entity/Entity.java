3aeffea115366c3505f3fefedcc619d6d52e097d
NKDuy <kn145660@gmail.com>
2023-02-05 17:05:20 +0700

    chore: Optimizing the Monster Class

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -8,6 +8,7 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * This stores variables that will be used in player, monster and NPC classes.
@@ -125,6 +126,31 @@ public class Entity {
 
     public int getRow() {
         return (worldY + solidArea.y) / gp.tileSize;
+    }
+
+    public int getXDistance(Entity target) {
+        int xDistance = Math.abs(worldX - target.worldX);
+        return xDistance;
+    }
+
+    public int getYDistance(Entity target) {
+        int yDistance = Math.abs(worldY - target.worldY);
+        return yDistance;
+    }
+
+    public int getTileDistance(Entity target) {
+        int tileDistance = (getXDistance(target) + getYDistance(target))/gp.tileSize;
+        return tileDistance;
+    }
+
+    public int getGoalCol(Entity target) {
+        int goalCol = (target.worldX + target.solidArea.x) / gp.tileSize;
+        return goalCol;
+    }
+
+    public int getGoalRow(Entity target) {
+        int goalRow = (target.worldY + target.solidArea.y) / gp.tileSize;
+        return goalRow;
     }
 
     public void setAction() {}
@@ -274,6 +300,59 @@ public class Entity {
 
         if (shotAvailableCounter < 30) {
             shotAvailableCounter++;
+        }
+    }
+
+    public void checkShootOrNot(int rate, int shotInterval) {
+        int i = new Random().nextInt(rate);
+        if (i == 0 && projectile.alive == false && shotAvailableCounter == shotInterval) {
+            projectile.set(worldX, worldY, direction, true, this);
+
+            // CHECK VACANCY
+            for (int ii = 0; ii < gp.projectile[1].length; ii++) {
+                if (gp.projectile[gp.currentMap][ii] == null) {
+                    gp.projectile[gp.currentMap][ii] = projectile;
+                    break;
+                }
+            }
+
+            shotAvailableCounter = 0;
+        }
+    }
+
+    public void checkStartChasingOrNot(Entity target, int distance, int rate) {
+        if (getTileDistance(target) < distance) {
+            int i = new Random().nextInt(rate);
+
+            if (i == 0) {
+                onPath = true;
+            }
+        }
+    }
+
+    public void checkStopChasingOrNot(Entity target, int distance, int rate) {
+        if (getTileDistance(target) > distance) {
+            int i = new Random().nextInt(rate);
+
+            if (i == 0) {
+                onPath = false;
+            }
+        }
+    }
+
+    public void getRandomDirection() {
+        actionLockCounter++;
+
+        if (actionLockCounter == 120) { // 2 second
+            Random random = new Random();
+            int i = random.nextInt(100) + 1; // pick up a number from 1 to 100
+
+            if (i <= 25) { direction = "up"; }
+            if (i > 25 && i <= 50) { direction = "down"; }
+            if (i > 50 && i <= 75) { direction = "left"; }
+            if (i > 75 && i <= 100) { direction = "right"; }
+
+            actionLockCounter = 0;
         }
     }
 
--- src/monster/MON_GreenSlime.java
+++ src/monster/MON_GreenSlime.java
@@ -51,67 +51,22 @@ public class MON_GreenSlime extends Entity {
         right2 = setup("/monster/greenslime_down_2", gp.tileSize, gp.tileSize);
     }
 
-    public void update() {
-        super.update();
-
-        int xDistance = Math.abs(worldX - gp.player.worldX);
-        int yDistance = Math.abs(worldY - gp.player.worldY);
-        int tileDistance = (xDistance + yDistance) / gp.tileSize;
-
-        if (onPath == false && tileDistance < 5) {
-            int i = new Random().nextInt(100) + 1;
-            if (i > 50) {
-                onPath = true;
-            }
-        }
-//        if (onPath == true && tileDistance > 20) {
-//            onPath = false;
-//        }
-    }
-
     public void setAction() {
         if (onPath == true) {
-            int goalCol = (gp.player.worldX + gp.player.solidArea.y) / gp.tileSize;
-            int goalRow = (gp.player.worldY + gp.player.solidArea.y) / gp.tileSize;
+            // Check if it stops chasing
+            checkStopChasingOrNot(gp.player, 15, 100);
 
-            searchPath(goalCol, goalRow);
+            // Search the direction to go
+            searchPath(getGoalCol(gp.player), getGoalRow(gp.player));
 
-            int i = new Random().nextInt(200) + 1;
-            if (i > 197 && projectile.alive == false && shotAvailableCounter == 30) {
-                projectile.set(worldX, worldY, direction, true, this);
-
-                // CHECK VACANCY
-//                for (int ii = 0; ii < gp.projectile[1].length; ii++) {
-//                    if (gp.projectile[gp.currentMap][ii] == null) {
-//                        gp.projectile[gp.currentMap][ii] = projectile;
-//                        break;
-//                    }
-//                }
-
-                shotAvailableCounter = 0;
-            }
+            // Check if it shoots a projectile
+            checkShootOrNot(200, 30);
         } else {
-            actionLockCounter++;
+            // Check if it starts chasing
+            checkStartChasingOrNot(gp.player, 5, 100);
 
-            if (actionLockCounter == 120) { // 2 second
-                Random random = new Random();
-                int i = random.nextInt(100) + 1; // pick up a number from 1 to 100
-
-                if (i <= 25) {
-                    direction = "up";
-                }
-                if (i > 25 && i <= 50) {
-                    direction = "down";
-                }
-                if (i > 50 && i <= 75) {
-                    direction = "left";
-                }
-                if (i > 75 && i <= 100) {
-                    direction = "right";
-                }
-
-                actionLockCounter = 0;
-            }
+            // Get a random direction
+            getRandomDirection();
         }
     }
 
