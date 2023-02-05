85003aa6a157a7899304f1e573ce952037f76b4d
NKDuy <kn145660@gmail.com>
2023-02-05 13:09:03 +0700

    chore: Creating Map Class

---
+++ src/tile/Map.java
@@ -0,0 +1,45 @@
+package tile;
+
+import main.GamePanel;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class Map extends TileManager {
+
+    GamePanel gp;
+    BufferedImage worldMap[];
+    public boolean miniMapOn = false;
+
+    public Map(GamePanel gp) {
+        super(gp);
+        this.gp = gp;
+    }
+
+    public void createWorldMap() {
+        worldMap = new BufferedImage[gp.maxMap];
+        int worldMapWidth = gp.tileSize * gp.maxWorldCol;
+        int worldMapHeight = gp.tileSize * gp.maxWorldRow;
+
+        for (int i = 0; i < gp.maxMap; i++) {
+            worldMap[i] = new BufferedImage(worldMapWidth, worldMapHeight, BufferedImage.TYPE_INT_ARGB);
+            Graphics2D g2 = (Graphics2D) worldMap[i].createGraphics();
+
+            int col = 0;
+            int row = 0;
+
+            while (col < gp.maxWorldCol && row < gp.maxWorldRow) {
+                int tileNum = mapTileNum[i][col][row];
+                int x = gp.tileSize * col;
+                int y = gp.tileSize * row;
+                g2.drawImage(tile[tileNum].image, x, y, null);
+
+                col++;
+                if (col == gp.maxWorldCol) {
+                    col = 0;
+                    row++;
+                }
+            }
+        }
+    }
+}
