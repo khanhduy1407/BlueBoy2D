0799da8ca3cf79b629de02d666377f5ee8781183
NKDuy <kn145660@gmail.com>
2023-02-07 12:39:06 +0700

    feat: new Advanced UI with Tile number

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -31,8 +31,8 @@ public class GamePanel extends JPanel implements Runnable {
     public final int screenHeight = tileSize * maxScreenRow; // 576 pixels
 
     // WORLD SETTINGS
-    public final int maxWorldCol = 50;
-    public final int maxWorldRow = 50;
+    public int maxWorldCol;
+    public int maxWorldRow;
     public final int maxMap = 10;
     public int currentMap = 0;
 
