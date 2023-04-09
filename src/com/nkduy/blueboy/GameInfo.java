f84c69eb7c4b06f1e12b1e82c36969badd5f435c
NKDuy <kn145660@gmail.com>
2023-04-09 15:36:31 +0700

    chore: Adding get git SHA method

--- src/com/nkduy/blueboy/GameInfo.java
+++ src/com/nkduy/blueboy/GameInfo.java
@@ -1,14 +1,21 @@
 package com.nkduy.blueboy;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class GameInfo {
 
     public String title = "Blue Boy Adventure";
+
+    String gitSHA = getGitSHA();
+    String shortGitSHA = gitSHA.substring(0, 7);
 
     int major = 3;
     int minor = 0;
     int patch = 0;
     String preRelease = "beta.0";
-    String build = "b2";
+    String build = shortGitSHA;
     public String version = major + "." + minor + "." + patch + "-" + preRelease + "+" + build;
     public boolean showVersion = false;
 
@@ -32,5 +39,23 @@ public class GameInfo {
         } else {
             Main.window.setTitle(title + " - " + subtitle  + " - " + version);
         }
+    }
+
+    private static String getGitSHA() {
+        String command = "git rev-parse HEAD";
+        Process process = null;
+        try {
+            process = Runtime.getRuntime().exec(command);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String line = null;
+        try {
+            line = reader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return line;
     }
 }
